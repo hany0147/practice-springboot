1255cfff4c40f5269aa236f721a8359db23336be
hany <hany0147@naver.com>
2024-05-21 15:52:29 +0900

    Initial commit

---
+++ src/test/java/com/hany1/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hany1.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
